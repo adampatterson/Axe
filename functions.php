7329dd7ba55faa202a1f0104301fdb0c9fe00f45
Adam Patterson <hello@adampatterson.ca>
2016-05-23 22:10:34 -0600

    .

--- functions.php
+++ functions.php
@@ -7,4 +7,13 @@ require_once( get_template_directory() . '/lib/bootstrap-walker.php' );
 require_once( get_template_directory() . '/lib/theme-helpers.php' );
 require_once( get_template_directory() . '/lib/theme-functions.php' );
 
+/**
+ * Load Jetpack compatibility file.
+ * See: http://jetpack.me/support/infinite-scroll/
+ * todo: adjust cotnetn classes
+ */
+//require get_template_directory() . '/lib/theme-jetpack.php';
+
+require_once( get_template_directory() . '/lib/theme-template-tags.php' );
+
 require_once( get_template_directory() . '/lib/custom.php' );
