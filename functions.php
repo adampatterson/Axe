bd91ba34b718cc2537be5d2c0b597bdd83de59b3
Adam Patterson <hello@adampatterson.ca>
2016-11-16 21:06:54 -0700

    simple.

--- functions.php
+++ functions.php
@@ -2,21 +2,21 @@
 /****************************************
  * Theme Setup
  *****************************************/
-require_once(get_template_directory() . '/lib/init.php');
-require_once(get_template_directory() . '/lib/theme-options.php');
-require_once(get_template_directory() . '/lib/bootstrap-walker.php');
-require_once(get_template_directory() . '/lib/theme-helpers.php');
-require_once(get_template_directory() . '/lib/theme-media.php');
-require_once(get_template_directory() . '/lib/theme-widgets.php');
-require_once(get_template_directory() . '/lib/theme-menus.php');
-require_once(get_template_directory() . '/lib/theme-functions.php');
-require_once(get_template_directory() . '/lib/theme-template-tags.php');
+require_once('lib/init.php');
+require_once('lib/theme-options.php');
+require_once('lib/bootstrap-walker.php');
+require_once('lib/theme-helpers.php');
+require_once('lib/theme-media.php');
+require_once('lib/theme-widgets.php');
+require_once('lib/theme-menus.php');
+require_once('lib/theme-functions.php');
+require_once('lib/theme-template-tags.php');
 
 /**
  * Load Jetpack compatibility file.
  * See: http://jetpack.me/support/infinite-scroll/
  * todo: adjust content classesw
  */
-//require get_template_directory() . '/lib/theme-jetpack.php';
+//require 'lib/theme-jetpack.php';
 
-require_once(get_template_directory() . '/lib/custom.php');
+require_once('lib/custom.php');
