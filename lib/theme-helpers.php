2a270ac96e39f92c380b7d451ad9d116e90f9fe1
Adam Patterson <hello@adampatterson.ca>
2016-11-29 21:46:18 -0700

    moved functions under custom to helpers

--- lib/theme-helpers.php
+++ lib/theme-helpers.php
@@ -24,3 +24,123 @@ function axe_add_capabilities($posttype)
     $role->add_cap('edit_private_' . $posttype . 's');
     $role->add_cap('edit_published_' . $posttype . 's');
 }
+
+function __t() {
+    return get_template_directory_uri() . '/';
+}
+
+function __a() {
+    return __t() . 'assets/';
+}
+
+function __j() {
+    echo __a() . 'js/';
+}
+
+function __i() {
+    echo __a() . 'img/';
+}
+
+function __c() {
+    echo __a() . 'css/';
+}
+
+function __v() {
+    echo __t() . 'vendor/';
+}
+
+function __video() {
+    echo __a() . 'video/';
+}
+
+/**
+ * @param $string
+ *
+ * @return string
+ */
+function underscore( $string ) {
+    return strtolower( preg_replace( '/(?<=\\w)([A-Z])/', '_\\1', $string ) );
+}
+
+/**
+ * @param $parent
+ * @param $args
+ *
+ * Category list
+ *
+ * @return array
+ */
+function get_cat_hierchy( $parent, $args ) {
+    $cats = get_categories( $args );
+    $ret  = new stdClass;
+
+    foreach ( $cats as $cat ) {
+        if ( $cat->parent == $parent ) {
+            $id                 = $cat->cat_ID;
+            $ret->$id           = $cat;
+            $ret->$id->children = get_cat_hierchy( $id, $args );
+        }
+    }
+
+    return (array) $ret;
+}
+
+/**
+ * @param      $slug
+ * @param null $name
+ * @param null $data
+ *
+ * Allows the passthrough of data to tempalte partials.
+ *
+ * @return string
+ */
+function get_template_part_acf( $slug, $name = null ) {
+    $templates = array();
+    $name      = (string) $name;
+
+    if ( $name == null ) {
+        $templates[] = "{$slug}.php";
+    } else {
+        $templates[] = "{$slug}-{$name}.php";
+    }
+
+    $located = '';
+    foreach ( (array) $templates as $template_name ) {
+        if ( ! $template_name ) {
+            continue;
+        }
+        if ( template_directory( $template_name ) ) {
+            $located = template_directory( $template_name );
+        }
+    }
+
+    return $located;
+}
+
+/**
+ * @param $template_name
+ *
+ * @return string
+ */
+function check_path( $template_name ) {
+    if ( file_exists( STYLESHEETPATH . '/' . $template_name ) or file_exists( TEMPLATEPATH . '/' . $template_name ) ) {
+        return TEMPLATEPATH . '/' . $template_name;
+    }
+}
+
+/**
+ * @param $template_name
+ *
+ * @return bool|string
+ */
+function template_directory( $template_name ) {
+    $template_name = trim( $template_name, "/" );
+
+    if ( file_exists( STYLESHEETPATH . '/' . $template_name ) ) {
+        return STYLESHEETPATH . '/' . $template_name;
+    }
+    if ( file_exists( TEMPLATEPATH . '/' . $template_name ) ) {
+        return TEMPLATEPATH . '/' . $template_name;
+    }
+    return false;
+}
