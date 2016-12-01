2c3d0e1d4d2a2757d4ad61e8cba64a37ea2b46b2
Adam Patterson <hello@adampatterson.ca>
2016-11-30 21:35:39 -0700

    footer links

--- templates/partials/footer.php
+++ templates/partials/footer.php
@@ -1,8 +1,21 @@
-    <footer>
-        <div class="container text-center">
-
-        </div>
-    </footer>
+<footer>
+    <div class="container text-center">
+        <?php
+        // http://code.tutsplus.com/tutorials/how-to-integrate-bootstrap-navbar-into-wordpress-theme--wp-33410
+        wp_nav_menu(array(
+                'menu'           => 'footer-links',
+                'theme_location' => 'footer-links',
+                'menu_id'        => 'footer-navigation',
+                'depth'          => 2,
+                'container'      => false,
+                'menu_class'     => 'nav navbar-nav primary-nav navbar-right',
+                'walker'         => new wp_bootstrap_navwalker()
+            )
+        );
+        #get_search_form();
+        ?>
+    </div>
+</footer>
 </div>
 <?php wp_footer(); ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
@@ -10,7 +23,7 @@
 
 <script type='text/javascript' src='<? __j() ?>app.js'></script>
 
-    <?php include( __DIR__ . '/admin-helper.php' ); ?>
+<?php include( get_template_part_acf( 'templates/partials/admin-helper' ) ); ?>
 
 </body>
 </html>
