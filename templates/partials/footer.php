d40760a046ead6f079c899c286ed566a4fe7a1c2
Adam Patterson <hello@adampatterson.ca>
2016-12-03 16:11:07 -0700

    closes #5

--- templates/partials/footer.php
+++ templates/partials/footer.php
@@ -18,9 +18,6 @@
 </footer>
 </div>
 <?php wp_footer(); ?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
 <script type='text/javascript' src='<? __j() ?>app.js'></script>
 
 <?php include( get_template_part_acf( 'templates/partials/admin-helper' ) ); ?>
