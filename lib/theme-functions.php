b56f4a386888f6d0143de92cbf9dfbae4df90907
Adam Patterson <hello@adampatterson.ca>
2016-12-03 16:35:17 -0700

    #7

--- lib/theme-functions.php
+++ lib/theme-functions.php
@@ -1,8 +1,8 @@
 <?php
 
 /****************************************
-Backend Functions
-*****************************************/
+ * Backend Functions
+ *****************************************/
 
 /**
  * Customize Contact Methods
@@ -12,14 +12,17 @@ Backend Functions
  * @link http://sillybean.net/2010/01/creating-a-user-directory-part-1-changing-user-contact-fields/
  *
  * @param array $contactmethods
+ *
  * @return array
  */
-function axe_contactmethods( $contactmethods ) {
-	unset( $contactmethods['aim'] );
-	unset( $contactmethods['yim'] );
-	unset( $contactmethods['jabber'] );
+if ( ! function_exists( 'axe_contactmethods' ) ) {
+    function axe_contactmethods( $contactmethods ) {
+        unset( $contactmethods['aim'] );
+        unset( $contactmethods['yim'] );
+        unset( $contactmethods['jabber'] );
 
-	return $contactmethods;
+        return $contactmethods;
+    }
 }
 
 /**
@@ -32,105 +35,130 @@ function axe_contactmethods( $contactmethods ) {
  * @author Mark Jaquith
  * @link http://markjaquith.wordpress.com/2009/12/14/excluding-your-plugin-or-theme-from-update-checks/
  *
- * @param array $r, request arguments
- * @param string $url, request url
+ * @param array  $r , request arguments
+ * @param string $url , request url
+ *
  * @return array request arguments
  */
-function axe_dont_update_theme( $r, $url ) {
-	if ( 0 !== strpos( $url, 'http://api.wordpress.org/themes/update-check' ) )
-		return $r; // Not a theme update request. Bail immediately.
-	$themes = unserialize( $r['body']['themes'] );
-	unset( $themes[ get_option( 'template' ) ] );
-	unset( $themes[ get_option( 'stylesheet' ) ] );
-	$r['body']['themes'] = serialize( $themes );
-	return $r;
+if ( ! function_exists( 'axe_dont_update_theme' ) ) {
+    function axe_dont_update_theme( $r, $url ) {
+        if ( 0 !== strpos( $url, 'http://api.wordpress.org/themes/update-check' ) ) {
+            return $r;
+        } // Not a theme update request. Bail immediately.
+        $themes = unserialize( $r['body']['themes'] );
+        unset( $themes[ get_option( 'template' ) ] );
+        unset( $themes[ get_option( 'stylesheet' ) ] );
+        $r['body']['themes'] = serialize( $themes );
+
+        return $r;
+    }
 }
 
 /**
  * Remove Dashboard Meta Boxes
  */
-function axe_remove_dashboard_widgets() {
-	global $wp_meta_boxes;
-	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_drafts']);
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
-	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
-	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_activity']);
+if ( ! function_exists( 'axe_remove_dashboard_widgets' ) ) {
+    function axe_remove_dashboard_widgets() {
+        global $wp_meta_boxes;
+        unset( $wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press'] );
+        unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links'] );
+        unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now'] );
+        unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins'] );
+        unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_drafts'] );
+        unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments'] );
+        unset( $wp_meta_boxes['dashboard']['side']['core']['dashboard_primary'] );
+        unset( $wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary'] );
+        unset( $wp_meta_boxes['dashboard']['normal']['core']['dashboard_activity'] );
+    }
 }
 
 /**
  * Change Admin Menu Order
  */
-function axe_custom_menu_order( $menu_ord ) {
-	if ( !$menu_ord ) return true;
-	return array(
-		 'index.php', // Dashboard
-		 'separator1', // First separator
-		 'edit.php?post_type=page', // Pages
-		 'edit.php', // Posts
-		 'upload.php', // Media
-		 'gf_edit_forms', // Gravity Forms
-		 'genesis', // Genesis
-		 'edit-comments.php', // Comments
-		 'separator2', // Second separator
-		 'themes.php', // Appearance
-		 'plugins.php', // Plugins
-		 'users.php', // Users
-		 'tools.php', // Tools
-		 'options-general.php', // Settings
-		 'separator-last', // Last separator
-	);
+if ( ! function_exists( 'axe_custom_menu_order' ) ) {
+    function axe_custom_menu_order( $menu_ord ) {
+        if ( ! $menu_ord ) {
+            return true;
+        }
+
+        return array(
+            'index.php', // Dashboard
+            'separator1', // First separator
+            'edit.php?post_type=page', // Pages
+            'edit.php', // Posts
+            'upload.php', // Media
+            'gf_edit_forms', // Gravity Forms
+            'genesis', // Genesis
+            'edit-comments.php', // Comments
+            'separator2', // Second separator
+            'themes.php', // Appearance
+            'plugins.php', // Plugins
+            'users.php', // Users
+            'tools.php', // Tools
+            'options-general.php', // Settings
+            'separator-last', // Last separator
+        );
+    }
 }
 
 /**
  * Hide Admin Areas that are not used
  */
-function axe_remove_menu_pages() {
-	remove_menu_page('link-manager.php');
+if ( ! function_exists( 'axe_remove_menu_pages' ) ) {
+    function axe_remove_menu_pages() {
+        remove_menu_page( 'link-manager.php' );
+    }
 }
 
 /**
  * Remove default link for images
  */
-function axe_imagelink_setup() {
-	$image_set = get_option( 'image_default_link_type' );
-	if ($image_set !== 'none') {
-		update_option('image_default_link_type', 'none');
-	}
+if ( ! function_exists( 'axe_imagelink_setup' ) ) {
+    function axe_imagelink_setup() {
+        $image_set = get_option( 'image_default_link_type' );
+        if ( $image_set !== 'none' ) {
+            update_option( 'image_default_link_type', 'none' );
+        }
+    }
 }
 
 /**
  * Show Kitchen Sink in WYSIWYG Editor
  */
-function axe_unhide_kitchensink( $args ) {
-	$args['wordpress_adv_hidden'] = false;
-	return $args;
+if ( ! function_exists( 'axe_unhide_kitchensink' ) ) {
+    function axe_unhide_kitchensink( $args ) {
+        $args['wordpress_adv_hidden'] = false;
+
+        return $args;
+    }
 }
 
 /**
  * Remove Query Strings From Static Resources
  */
-function axe_remove_script_version( $src ){
-	$parts = explode( '?', $src );
-	return $parts[0];
+if ( ! function_exists( 'axe_remove_script_version' ) ) {
+    function axe_remove_script_version( $src ) {
+        $parts = explode( '?', $src );
+
+        return $parts[0];
+    }
 }
 
 /**
  * Remove Read More Jump
  */
-function axe_remove_more_jump_link( $link ) {
-	$offset = strpos( $link, '#more-' );
-	if ($offset) {
-		$end = strpos( $link, '"',$offset );
-	}
-	if ($end) {
-		$link = substr_replace( $link, '', $offset, $end-$offset );
-	}
-	return $link;
+if ( ! function_exists( 'axe_remove_more_jump_link' ) ) {
+    function axe_remove_more_jump_link( $link ) {
+        $offset = strpos( $link, '#more-' );
+        if ( $offset ) {
+            $end = strpos( $link, '"', $offset );
+        }
+        if ( $end ) {
+            $link = substr_replace( $link, '', $offset, $end - $offset );
+        }
+
+        return $link;
+    }
 }
 
 show_admin_bar( false );
