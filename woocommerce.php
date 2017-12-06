5d2a401157e409bf7079f074c6efd074413901b2
Adam Patterson <hello@adampatterson.ca>
2017-12-06 13:19:44 -0700

    Woo template rename

--- woocommerce.php
+++ woocommerce.php
@@ -3,6 +3,6 @@ $data = get_fields();
 include( get_template_part_acf( 'templates/partials/header' ) );
 
 echo '<!-- template: woo/page -->';
-include( get_template_part_acf( 'templates/woo', 'product' ) );
+include( get_template_part_acf( 'templates/woo', 'product-listing' ) );
 
 include( get_template_part_acf( 'templates/partials/footer' ) );
