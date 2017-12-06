300201f2130a138db585baed71f9a356d5cdace5
Adam Patterson <hello@adampatterson.ca>
2017-12-06 13:19:56 -0700

    archive and admin helper cleanup

--- archive.php
+++ archive.php
@@ -2,20 +2,21 @@
 /**
  * Template Name: Archives
  */
-
 $data = get_fields();
-include( get_template_part_acf( 'templates/partials/header' ) );
 
-if ( have_posts() ):
-    $terms    = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
-    $category = $terms->taxonomy;
-    if ( check_path( get_template_directory() . '/templates/archive-' . $category . '.php' ) ):
+include(get_template_part_acf('templates/partials/header'));
+
+if (have_posts()):
+    $terms    = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
+    $category = ($terms) ? $terms->taxonomy : null;
+
+    if (check_path('/templates/archive-' . $category . '.php')):
         echo '<!-- template: index/archive-' . $category . ' -->';
-        include( get_template_part_acf( 'templates/archive', $category ) );
+        include(get_template_part_acf('templates/archive', $category));
     else:
         echo '<!-- template: index/archive -->';
-        include( get_template_part_acf( 'templates/archive', 'default' ) );
+        include(get_template_part_acf('templates/archive', 'default'));
     endif;
 endif;
 
-include( get_template_part_acf( 'templates/partials/footer' ) );
+include(get_template_part_acf('templates/partials/footer'));
--- templates/partials/admin-helper.php
+++ templates/partials/admin-helper.php
@@ -1,5 +1,13 @@
-<?php if (is_user_logged_in() and isset($data)): ?>
-    <script>
-        console.log(<?= json_encode($data) ?>)
-    </script>
-<?php endif; ?>
+<?php if (is_user_logged_in()):
+    if (isset($data)): ?>
+        <script>
+            console.log(<?= json_encode($data) ?>)
+        </script>
+    <?php endif;
+
+    if (isset($products)): ?>
+        <script>
+            console.log(<?= json_encode($products) ?>)
+        </script>
+    <?php endif;
+endif; ?>
