2009f806c9305bff2914e864068f4b4376f7995e
Adam Patterson <hello@adampatterson.ca>
2018-01-24 00:18:23 -0700

    .

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -1,40 +1,42 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
     <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name') ?></title>
 
-    <script type='application/ld+json'>{"@context":"http:\/\/schema.org","@type":"WebSite","url":"<?= home_url('/'); ?>","name":"<?= bloginfo('name') ?>","potentialAction":{"@type":"SearchAction","target":"<?= home_url('/'); ?>?s={search_term}","query-input":"required name=search_term"}}</script>
+    <script
+        type='application/ld+json'>{"@context":"http:\/\/schema.org","@type":"WebSite","url":"<?= home_url('/'); ?>","name":"<?= bloginfo('name') ?>","potentialAction":{"@type":"SearchAction","target":"<?= home_url('/'); ?>?s={search_term}","query-input":"required name=search_term"}}
+    </script>
 
     <?php wp_head(); ?>
     <link rel="stylesheet" href="<?= __c() ?>base.css"/>
 </head>
 <?php
-if (is_null($post)){
-    $post = (object)$post;
+if (is_null($post)) {
+    $post            = (object)$post;
     $post->post_name = '';
 }
 ?>
 <body <?php body_class($post->post_name); ?>>
 <script>
-    (function(i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function() {
-                (i[r].q = i[r].q || []).push(arguments)
-            }, i[r].l = 1 * new Date();
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r
+        i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date()
         a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
+            m = s.getElementsByTagName(o)[0]
+        a.async = 1
+        a.src = g
         m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga')
 
-    ga('create', '{@todo: GA Tracking}', 'auto');
-    ga('send', 'pageview');
+    ga('create', '{@todo: GA Tracking}', 'auto')
+    ga('send', 'pageview')
 </script>
 <div id="wrapper">
     <div class="navbar-wrapper">
@@ -71,16 +73,13 @@ if (is_null($post)){
                 <div class="container">
                     <!-- Brand and toggle get grouped for better mobile display -->
                     <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu" aria-expanded="false">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu"
+                                aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span
+                                class="icon-bar"></span> <span class="icon-bar"></span>
                         </button>
                         <div class="navbar-brand nav-logo pull-left">
-                            <a href="<?= home_url( '/' ) ?>">
-                                <img src="<?php __i() ?>logo.png" alt="">
-                            </a>
+                            <a href="<?= home_url('/') ?>"> <img src="<?php __i() ?>logo.png" alt=""> </a>
                         </div>
                     </div>
 
@@ -90,16 +89,15 @@ if (is_null($post)){
                         // http://code.tutsplus.com/tutorials/how-to-integrate-bootstrap-navbar-into-wordpress-theme--wp-33410
                         // https://github.com/jeffmould/multi-level-bootstrap-menu/blob/master/wp-bootstrap-navwalker.php
                         if (has_nav_menu('main-menu')) {
-                        wp_nav_menu(array(
-                                'menu'           => 'main-menu',
-                                'theme_location' => 'main-menu',
-                                'menu_id'        => 'navigation',
-                                'depth'          => 3,
-                                'container'      => false,
-                                'menu_class'     => 'nav navbar-nav primary-nav navbar-right',
-                                'walker'         => new wp_bootstrap_navwalker()
-                            )
-                        );
+                            wp_nav_menu(array(
+                                    'menu'           => 'main-menu',
+                                    'theme_location' => 'main-menu',
+                                    'menu_id'        => 'navigation',
+                                    'depth'          => 3,
+                                    'container'      => false,
+                                    'menu_class'     => 'nav navbar-nav primary-nav navbar-right',
+                                    'walker'         => new wp_bootstrap_navwalker()
+                                ));
                         }
                         #get_search_form();
                         ?>
@@ -111,7 +109,7 @@ if (is_null($post)){
         </div>
     </div>
 
-<?php /*
+    <?php /*
      <div id="sidebar">
         <?php get_template_part('templates/partials/sidebar'); ?>
     </div>
