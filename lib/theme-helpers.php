8a98f86e5d19ae412c64277d1e549e7c76d32cb1
Adam Patterson <hello@adampatterson.ca>
2018-01-24 23:34:53 -0700

    adds function_exists to the __ helpers

--- lib/theme-helpers.php
+++ lib/theme-helpers.php
@@ -7,53 +7,73 @@
 /**
  * Add capabilities for a custom post type
  */
-if ( ! function_exists( 'axe_add_capabilities' ) ) {
-    function axe_add_capabilities( $posttype ) {
+if ( ! function_exists('axe_add_capabilities')) {
+    function axe_add_capabilities( $posttype )
+    {
         // gets the author role
-        $role = get_role( 'administrator' );
+        $role = get_role('administrator');
 
         // adds all capabilities for a given post type to the administrator role
-        $role->add_cap( 'edit_' . $posttype . 's' );
-        $role->add_cap( 'edit_others_' . $posttype . 's' );
-        $role->add_cap( 'publish_' . $posttype . 's' );
-        $role->add_cap( 'read_private_' . $posttype . 's' );
-        $role->add_cap( 'delete_' . $posttype . 's' );
-        $role->add_cap( 'delete_private_' . $posttype . 's' );
-        $role->add_cap( 'delete_published_' . $posttype . 's' );
-        $role->add_cap( 'delete_others_' . $posttype . 's' );
-        $role->add_cap( 'edit_private_' . $posttype . 's' );
-        $role->add_cap( 'edit_published_' . $posttype . 's' );
+        $role->add_cap('edit_' . $posttype . 's');
+        $role->add_cap('edit_others_' . $posttype . 's');
+        $role->add_cap('publish_' . $posttype . 's');
+        $role->add_cap('read_private_' . $posttype . 's');
+        $role->add_cap('delete_' . $posttype . 's');
+        $role->add_cap('delete_private_' . $posttype . 's');
+        $role->add_cap('delete_published_' . $posttype . 's');
+        $role->add_cap('delete_others_' . $posttype . 's');
+        $role->add_cap('edit_private_' . $posttype . 's');
+        $role->add_cap('edit_published_' . $posttype . 's');
     }
 }
 
-if ( ! function_exists( '__t' ) ) {
-    function __t() {
+if ( ! function_exists('__t')) {
+    function __t()
+    {
         return get_template_directory_uri() . '/';
     }
 }
 
-function __a() {
-    return __t() . 'assets/';
+if ( ! function_exists('__a')) {
+    function __a()
+    {
+        return __t() . 'assets/';
+    }
 }
 
-function __j() {
-    echo __a() . 'js/';
+if ( ! function_exists('__j')) {
+    function __j()
+    {
+        echo __a() . 'js/';
+    }
 }
 
-function __i() {
-    echo __a() . 'img/';
+if ( ! function_exists('__i')) {
+    function __i()
+    {
+        echo __a() . 'img/';
+    }
 }
 
-function __c() {
-    echo __a() . 'css/';
+if ( ! function_exists('__c')) {
+    function __c()
+    {
+        echo __a() . 'css/';
+    }
 }
 
-function __v() {
-    echo __t() . 'vendor/';
+if ( ! function_exists('__v')) {
+    function __v()
+    {
+        echo __t() . 'vendor/';
+    }
 }
 
-function __video() {
-    echo __a() . 'video/';
+if ( ! function_exists('__video')) {
+    function __video()
+    {
+        echo __a() . 'video/';
+    }
 }
 
 /**
@@ -61,8 +81,9 @@ function __video() {
  *
  * @return string
  */
-function underscore( $string ) {
-    return strtolower( preg_replace( '/(?<=\\w)([A-Z])/', '_\\1', $string ) );
+function underscore( $string )
+{
+    return strtolower(preg_replace('/(?<=\\w)([A-Z])/', '_\\1', $string));
 }
 
 /**
@@ -73,19 +94,20 @@ function underscore( $string ) {
  *
  * @return array
  */
-function get_cat_hierchy( $parent, $args ) {
-    $cats = get_categories( $args );
+function get_cat_hierchy( $parent, $args )
+{
+    $cats = get_categories($args);
     $ret  = new stdClass;
 
-    foreach ( $cats as $cat ) {
-        if ( $cat->parent == $parent ) {
+    foreach ($cats as $cat) {
+        if ($cat->parent == $parent) {
             $id                 = $cat->cat_ID;
             $ret->$id           = $cat;
-            $ret->$id->children = get_cat_hierchy( $id, $args );
+            $ret->$id->children = get_cat_hierchy($id, $args);
         }
     }
 
-    return (array) $ret;
+    return (array)$ret;
 }
 
 /**
@@ -97,23 +119,24 @@ function get_cat_hierchy( $parent, $args ) {
  *
  * @return string
  */
-function get_template_part_acf( $slug, $name = null ) {
+function get_template_part_acf( $slug, $name = null )
+{
     $templates = array();
-    $name      = (string) $name;
+    $name      = (string)$name;
 
-    if ( $name == null ) {
+    if ($name == null) {
         $templates[] = "{$slug}.php";
     } else {
         $templates[] = "{$slug}-{$name}.php";
     }
 
     $located = '';
-    foreach ( (array) $templates as $template_name ) {
-        if ( ! $template_name ) {
+    foreach ((array)$templates as $template_name) {
+        if ( ! $template_name) {
             continue;
         }
-        if ( template_directory( $template_name ) ) {
-            $located = template_directory( $template_name );
+        if (template_directory($template_name)) {
+            $located = template_directory($template_name);
         }
     }
 
@@ -125,8 +148,9 @@ function get_template_part_acf( $slug, $name = null ) {
  *
  * @return string
  */
-function check_path( $template_name ) {
-    if ( file_exists( STYLESHEETPATH . '/' . $template_name ) or file_exists( TEMPLATEPATH . '/' . $template_name ) ) {
+function check_path( $template_name )
+{
+    if (file_exists(STYLESHEETPATH . '/' . $template_name) or file_exists(TEMPLATEPATH . '/' . $template_name)) {
         return TEMPLATEPATH . '/' . $template_name;
     }
 }
@@ -136,13 +160,14 @@ function check_path( $template_name ) {
  *
  * @return bool|string
  */
-function template_directory( $template_name ) {
-    $template_name = trim( $template_name, "/" );
+function template_directory( $template_name )
+{
+    $template_name = trim($template_name, "/");
 
-    if ( file_exists( STYLESHEETPATH . '/' . $template_name ) ) {
+    if (file_exists(STYLESHEETPATH . '/' . $template_name)) {
         return STYLESHEETPATH . '/' . $template_name;
     }
-    if ( file_exists( TEMPLATEPATH . '/' . $template_name ) ) {
+    if (file_exists(TEMPLATEPATH . '/' . $template_name)) {
         return TEMPLATEPATH . '/' . $template_name;
     }
 
