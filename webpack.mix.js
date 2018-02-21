076e8e520fd075fc3721d55e2c78a4aff6b9a946
Adam Patterson <hello@adampatterson.ca>
2018-02-20 22:46:12 -0700

    Mix Versioning

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,17 @@
-let mix = require( 'laravel-mix' ).mix;
+let mix = require('laravel-mix')
 
 let scssOptions = {
     processCssUrls: false
-};
+}
 
 let bundles = {
     'all': [
         './src/js/app.js'
     ]
-};
+}
 
-mix.scripts( bundles.all, 'assets/js/app.js' ).
-    sass( 'src/scss/base.scss', 'assets/css' ).options( scssOptions );
+mix
+    .setPublicPath('./')
+    .sass('src/scss/base.scss', 'assets/css').options(scssOptions)
+    .scripts(bundles.all, 'assets/js/app.js')
+    .version()
