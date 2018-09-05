d76fbb2feee7606568fd77e1fa9aea972fec4888
Adam Patterson <hello@adampatterson.ca>
2018-09-04 20:31:29 -0600

    Fixes single pages to default back to a content template

--- single.php
+++ single.php
@@ -1,20 +1,22 @@
 <?php
 $data = get_fields();
 
-include( get_template_part_acf( 'templates/partials/header' ) );
+include(get_template_part_acf('templates/partials/header'));
 
-if ( have_posts() ):
-    while ( have_posts() ):
+if (have_posts()):
+    while (have_posts()):
         the_post();
 
-        if ( get_post_type() == 'post' && ! get_post_format() ):
-            include( get_template_part_acf( 'templates/format', 'standard' ) );
-        elseif ( get_post_type() == 'post' ):
-            include( get_template_part_acf( 'templates/format', get_post_format() ) );
+        if (get_post_type() == 'post' && ! get_post_format()):
+            include(get_template_part_acf('templates/format', 'standard'));
+        elseif (get_post_type() == 'post'):
+            include(get_template_part_acf('templates/format', get_post_format()));
+        elseif (check_path('/templates/content-' . get_post_type() . '.php')):
+            include(get_template_part_acf('templates/content', get_post_type()));
         else:
-            include( get_template_part_acf( 'templates/single', get_post_type() ) );
+            include(get_template_part_acf('templates/content', 'single'));
         endif;
     endwhile;
 endif;
 
-include( get_template_part_acf( 'templates/partials/footer' ) );
+include(get_template_part_acf('templates/partials/footer'));
