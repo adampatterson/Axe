025e8709f5001184500852f7a755e74a77ff8725
Adam Patterson <hello@adampatterson.ca>
2018-10-21 21:21:49 -0600

    adds is_sub_page()

--- page.php
+++ page.php
@@ -3,12 +3,17 @@ $data = get_fields();
 
 include(get_template_part_acf('templates/partials/header'));
 
+echo '<!-- master/page -->';
 while (have_posts()) : the_post();
     if (is_front_page()):
         echo '<!-- template: templates/content-home -->';
         include(get_template_part_acf('templates/content', 'home'));
     else:
-        if (check_path('/templates/content-' . $post->post_name . '.php')):
+        if (is_sub_page($post)):
+            $patent = get_post($post->post_parent);
+            echo '<!-- template: templates/sub-' . $patent->post_name . ' -->';
+            include(get_template_part_acf('templates/sub', $patent->post_name));
+        elseif (check_path('/templates/content-' . $post->post_name . '.php')):
             echo '<!-- template: templates/content-' . $post->post_name . ' -->';
             include(get_template_part_acf('templates/content', $post->post_name));
         else:
