b7f9acabfcde66d2d5410292d943202f38d867b8
Adam Patterson <hello@adampatterson.ca>
2019-02-27 09:55:37 -0700

    Cleanup, removes required plugins.

--- functions.php
+++ functions.php
@@ -3,7 +3,6 @@
 // require 'vendor/autoload.php';
 
 // Theme Setup
-require_once 'lib/plugins/class-tgm-plugin-activation.php';
 require_once( 'lib/init.php' );
 require_once( 'lib/theme-options.php' );
 require_once( 'lib/bootstrap-walker.php' );
@@ -17,11 +16,8 @@ require_once( 'lib/theme-template-tags.php' );
 /**
  * Load Jetpack compatibility file.
  * See: http://jetpack.me/support/infinite-scroll/
- * todo: adjust content classesw
+ * todo: adjust content classes
  */
 //require 'lib/theme-jetpack.php';
 
 require_once( 'lib/custom.php' );
-
-// update_option('siteurl','http://example.com');
-// update_option('home','http://example.com');
