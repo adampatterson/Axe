a627211daddd9eae703d0574a03d65ac227ff873
Adam Patterson <hello@adampatterson.ca>
2019-04-24 11:18:33 -0600

    WebPack cleanup, adds BrowserSync

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,23 +1,67 @@
-let mix = require( 'laravel-mix' );
+let mix = require('laravel-mix')
 
 let scssOptions = {
-  processCssUrls: false
-};
+    processCssUrls: false
+}
 
 let bundles = {
-  'all': [
-    './src/js/app.js'
-  ]
-};
+    'all': [
+        './src/js/app.js'
+    ]
+}
 
 let extractLibs = [
-  'jquery'
-];
+    'jquery'
+]
 
-mix.setPublicPath( './' ).
-    sass( 'src/scss/base.scss', 'assets/css' ).
-    options( scssOptions )
-    // Extract libraries requires ECMAScript 6 imports in your code.
-    // .js(bundles.all, 'assets/js/app.js').extract(extractLibs)
-    .js( bundles.all, 'assets/js/app.js' ).
-    version();
+mix.autoload({
+    'jquery': ['$', 'window.jQuery', 'jQuery']
+})
+
+mix.setPublicPath('./')
+
+mix.sass('src/scss/base.scss', 'assets/css').options(scssOptions)
+// Extract libraries requires ECMAScript 6 imports in your code.
+// mix.js(bundles.all, 'assets/js/app.js').extract(extractLibs)
+
+mix.js(bundles.all, 'assets/js/app.js').version()
+
+
+// Production
+if (mix.inProduction()) {
+    mix.options({
+        terser: {
+            terserOptions: {
+                warnings: true
+            }
+        }
+    })
+} else {
+    mix.webpackConfig({
+        devtool: 'source-map',
+    })
+
+    mix.sourceMaps()
+}
+
+// BrowserSync and LiveReload on `npm run watch` command
+//mix.browserSync({
+//    proxy: 'http://wordpress.local',
+//    port: 8080,
+//    If you want to use HTTPS, update the proxy path
+//    and add tour local certificate paths
+//    https: {
+//        key: '/your/certificates/location/your-local-domain.key',
+//        cert: '/your/certificates/location/your-local-domain.crt'
+//    },
+//    files: [
+//        '**/*.php',
+//        'assets/css/**/*.css',
+//        'assets/js/**/*.js',
+//        '!node_modules',
+//        '!vendor',
+//    ],
+//    ghostMode: false,
+//    open: false,
+//    injectChanges: true,
+//})
