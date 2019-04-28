6f3bec82d63bea8e21d5c38f2d8914d9aa5e410e
Adam Patterson <hello@adampatterson.ca>
2019-04-27 21:50:06 -0600

    post format / type routing fix

--- single.php
+++ single.php
@@ -8,17 +8,17 @@ if (have_posts()):
     while (have_posts()):
         the_post();
 
-        if (get_post_type() == 'post' && ! get_post_format()):
-            echo '<!-- template: templates/format-standard -->';
-            include(get_template_part_acf('templates/format', 'standard'));
-
-        elseif (check_path('/templates/format-' . get_post_format() . '.php')):
+        if (check_path('/templates/format-' . get_post_format() . '.php')):
             echo '<!-- template: templates/format-' . get_post_format() . ' -->';
             include(get_template_part_acf('templates/format', get_post_format()));
 
         elseif (check_path('/templates/single-' . get_post_type() . '.php')):
             echo '<!-- template: templates/single-' . get_post_type() . ' -->';
             include(get_template_part_acf('templates/single', get_post_type()));
+
+        elseif (get_post_type() == 'post' && get_post_format()):
+            echo '<!-- template: templates/format-standard -->';
+            include(get_template_part_acf('templates/format', 'standard'));
 
         else:
             echo '<!-- template: templates/content-single -->';
