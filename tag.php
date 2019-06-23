5b5cede163d42db89805edd3c4f3225318375239
Adam Patterson <hello@adampatterson.ca>
2019-06-22 20:39:00 -0600

    Fixes tempalte include

--- tag.php
+++ tag.php
@@ -1,5 +1,5 @@
-<?
-include(get_template_part('templates/partials/header'));
+<?php
+include(get_template_part_acf('templates/partials/header'));
 
 $term_id  = get_query_var('tag_id');
 $taxonomy = 'post_tag';
@@ -18,5 +18,4 @@ else:
     include(get_template_part_acf('templates/archive', 'tag'));
 endif;
 
-
-include(get_template_part('templates/partials/footer'));
+include(get_template_part_acf('templates/partials/footer'));
