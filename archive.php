7cae3368a000d910538794412a6401ab95e9acfa
Adam Patterson <hello@adampatterson.ca>
2019-06-24 14:53:44 -0600

    #15 adds archive

---
+++ archive.php
@@ -0,0 +1,32 @@
+<?php
+$data = get_fields();
+$post = get_post();
+
+include(get_template_part_acf('templates/partials/header'));
+
+echo '<!-- master/archive -->';
+
+if (have_posts()):
+    if (is_archive()):
+        $post_type = get_post_type();
+
+        if ($post_type) {
+            $post_type_data = get_post_type_object($post_type);
+            $post_type_slug = $post_type_data->rewrite['slug'];
+        }
+
+        if (check_path('/templates/archive-' . $post_type_slug . '.php')):
+            echo '<!-- template: index/archive-' . $post_type_slug . ' -->';
+            include(get_template_part_acf('templates/archive', $post_type_slug));
+        else:
+            echo '<!-- template: index/archive -->';
+            include(get_template_part_acf('templates/archive', 'default'));
+        endif;
+    endif;
+
+else:
+    echo '<!-- template: index/no_posts -->';
+    include(get_template_part_acf('templates/archive', 'default'));
+endif;
+
+include(get_template_part_acf('templates/partials/footer'));
--- index.php
+++ index.php
@@ -5,13 +5,13 @@ $post = get_post();
 include(get_template_part_acf('templates/partials/header'));
 
 echo '<!-- master/index -->';
+
 if (have_posts()):
     # Sort this out, Blog is not loading
     if (is_front_page() or is_home()):
         echo '<!-- template: index/blog -->';
         include(get_template_part_acf('templates/content', 'blog'));
     endif;
-
 else:
     echo '<!-- template: index/no_posts -->';
     include(get_template_part_acf('templates/none'));
