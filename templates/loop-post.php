cd50b18e9a2a3ed888c78c22199aa18ebbe87504
Adam Patterson <hello@adampatterson.ca>
2019-06-24 20:12:15 -0600

    .

--- templates/loop-post.php
+++ templates/loop-post.php
@@ -1,7 +1,7 @@
 <?php while (have_posts()) : the_post(); ?>
     <article <?php post_class('pb-5'); ?> itemscope="" itemprop="blogPost" itemtype="http://schema.org/BlogPosting">
         <?php if (has_post_thumbnail()) { ?>
-            <div class="post-image pb-1">
+            <div class="post-image pb-3">
                 <a href="<?php the_permalink(); ?>" title="Read Full Post">
                     <?php the_post_thumbnail('featured', array(
                         'class' => "img-fluid",
