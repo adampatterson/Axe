fb914eccb3302ba54ea2dcc310a36fab3fc813b1
Adam Patterson <hello@adampatterson.ca>
2019-06-26 21:00:39 -0600

    re-enabled some old widgets

--- lib/Core/Widgets.php
+++ lib/Core/Widgets.php
@@ -43,18 +43,12 @@ class Widgets
     {
         unregister_widget('WP_Widget_Pages');
         unregister_widget('WP_Widget_Calendar');
-        unregister_widget('WP_Widget_Archives');
         unregister_widget('WP_Widget_Links');
         unregister_widget('WP_Widget_Meta');
         unregister_widget('WP_Widget_Search');
-        unregister_widget('WP_Widget_Text');
-        unregister_widget('WP_Widget_Categories');
-        unregister_widget('WP_Widget_Recent_Posts');
         unregister_widget('WP_Widget_Recent_Comments');
-        unregister_widget('WP_Widget_RSS');
         unregister_widget('WP_Widget_Tag_Cloud');
         unregister_widget('WP_Nav_Menu_Widget');
-        unregister_widget('Twenty_Eleven_Ephemera_Widget');
     }
 
 }
