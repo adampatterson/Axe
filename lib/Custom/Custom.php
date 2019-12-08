edffb16211f082c15802165446d5369fead45fce
Adam Patterson <hello@adampatterson.ca>
2019-12-08 13:27:43 -0700

    moved add_custom_link_into_appearance_menu to Custom code.

--- lib/Custom/Custom.php
+++ lib/Custom/Custom.php
@@ -5,4 +5,18 @@ namespace Axe\Custom;
 class Custom
 {
 
+    public function __construct()
+    {
+
+        // Places a link under Appearance in order to manage reusable Gutenberg Blocks.
+        add_action('admin_menu', [$this, 'add_custom_link_into_appearance_menu']);
+    }
+
+    public function add_custom_link_into_appearance_menu()
+    {
+        global $submenu;
+        $blockLink             = 'edit.php?post_type=wp_block';
+        $submenu['edit.php'][] = array('Edit Reusable Blocks', 'manage_options', $blockLink);
+    }
+
 }
