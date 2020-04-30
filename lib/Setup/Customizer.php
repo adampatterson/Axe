e45ee3247a56a2970f391af8c10555bbdc68025d
Adam Patterson <hello@adampatterson.ca>
2020-04-30 14:47:45 -0600

    .

--- lib/Setup/Customizer.php
+++ lib/Setup/Customizer.php
@@ -24,46 +24,49 @@ class Customizer
 
     public function axe_theme_customize_register($wp_customize)
     {
-// Theme layout settings.
-        $wp_customize->add_section('understrap_theme_layout_options', array(
+        // Theme layout settings.
+        $wp_customize->add_section('axe_theme_layout_options', array(
             'title'       => __('Theme Layout Settings', 'axe'),
             'capability'  => 'edit_theme_options',
             'description' => __('Container width and sidebar defaults', 'axe'),
             'priority'    => 160,
         ));
 
-        $wp_customize->add_setting('understrap_container_type', array(
+        // Container Size
+        $wp_customize->add_setting('axe_container_type', array(
             'default'           => 'container',
             'type'              => 'theme_mod',
             'sanitize_callback' => [$this, 'axe_theme_slug_sanitize_select'],
             'capability'        => 'edit_theme_options',
         ));
 
-        $wp_customize->add_control(new \WP_Customize_Control($wp_customize, 'understrap_container_type', array(
+        $wp_customize->add_control(new \WP_Customize_Control($wp_customize, 'axe_container_type', array(
             'label'       => __('Container Width', 'axe'),
             'description' => __('Choose between Bootstrap\'s container and container-fluid', 'axe'),
-            'section'     => 'understrap_theme_layout_options',
-            'settings'    => 'understrap_container_type',
+            'section'     => 'axe_theme_layout_options',
+            'settings'    => 'axe_container_type',
             'type'        => 'select',
             'choices'     => array(
-                'container'       => __('Fixed width container', 'axe'),
-                'container-fluid' => __('Full width container', 'axe'),
+                'container'              => __('Fixed width container', 'axe'),
+                'container container-xl' => __('Fixed extra wide container', 'axe'),
+                'container-fluid'        => __('Full width container', 'axe'),
             ),
             'priority'    => '10',
         )));
 
-        $wp_customize->add_setting('understrap_sidebar_position', array(
+        // Sidebar Positioning
+        $wp_customize->add_setting('axe_sidebar_position', array(
             'default'           => 'right',
             'type'              => 'theme_mod',
             'sanitize_callback' => 'sanitize_text_field',
             'capability'        => 'edit_theme_options',
         ));
 
-        $wp_customize->add_control(new \WP_Customize_Control($wp_customize, 'understrap_sidebar_position', array(
+        $wp_customize->add_control(new \WP_Customize_Control($wp_customize, 'axe_sidebar_position', array(
             'label'             => __('Sidebar Positioning', 'axe'),
             'description'       => __('Set sidebar\'s default position. Can either be: right, left, both or none. Note: this can be overridden on individual pages.', 'axe'),
-            'section'           => 'understrap_theme_layout_options',
-            'settings'          => 'understrap_sidebar_position',
+            'section'           => 'axe_theme_layout_options',
+            'settings'          => 'axe_sidebar_position',
             'type'              => 'select',
             'sanitize_callback' => [$this, 'axe_theme_slug_sanitize_select'],
             'choices'           => array(
