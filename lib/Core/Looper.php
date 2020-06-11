9a61a2b79941245570a37e32cff3ba314b566c1c
Adam Patterson <hello@adampatterson.ca>
2020-06-11 11:17:10 -0600

    Looper Docs

--- lib/Core/Looper.php
+++ lib/Core/Looper.php
@@ -2,18 +2,38 @@
 
 namespace Axe\Core;
 
+/**
+ * Class Looper
+ * @package Axe\Core
+ */
 class Looper
 {
 
+    /**
+     * @var int
+     */
     public $index = 0;
+
+    /**
+     * @var string|void|\WP_Query
+     */
     private $wp_query;
 
+    /**
+     * Pulls the $wp_query from the Global scope.
+     */
     public function __construct()
     {
         global $wp_query;
         $this->wp_query = $wp_query;
     }
 
+    /**
+     * Exactly the same as have_posts() but also serves
+     * as a tidy place to increment the loop.
+     *
+     * @return bool
+     */
     public function have_posts()
     {
         $this->iterate();
@@ -21,31 +41,51 @@ class Looper
         return $this->wp_query->have_posts();
     }
 
+    /**
+     * Sets and increments the current index.
+     */
     public function iterate()
     {
         $this->index = $this->index + 1;
     }
 
+    /**
+     * Returns the current post index on the page.
+     *
+     * @return int
+     */
     public function index()
     {
         return $this->index;
     }
 
+    /**
+     * @return bool
+     */
     public function first()
     {
         return ($this->index == 1) ? true : false;
     }
 
+    /**
+     * @return bool
+     */
     public function last()
     {
         return ($this->index == count($this->wp_query->posts)) ? true : false;
     }
 
+    /**
+     * @return bool
+     */
     public function even()
     {
         return $this->index % 2 == 0;
     }
 
+    /**
+     * @return bool
+     */
     public function odd()
     {
         return $this->index % 2 !== 0;
