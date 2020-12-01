30817116f5d87f1c71bbdc8b0a82aa037b0afbdd
Adam Patterson <hello@adampatterson.ca>
2020-12-01 14:46:20 -0700

    .

--- lib/Core/Media.php
+++ lib/Core/Media.php
@@ -2,31 +2,31 @@
 
 namespace Axe\Core;
 
-class Media {
+class Media
+{
 
-    public function __construct() {
-        add_image_size( 'featured', 1600, 600, true ); // Without Blur
-        add_image_size( 'gallery', 625, 1000 );
-        add_image_size( 'grid', 625, 625, true );
-        add_image_size( 'square', 150, 150, true );
-        add_image_size( 'sidebar', 470, 353, true );
-        add_image_size( 'post', 250, 250, true );
+    public function __construct()
+    {
+        add_image_size('featured', 1600, 600, true); // Without Blur
+        add_image_size('gallery', 625, 1000);
+        add_image_size('grid', 625, 625, true);
+        add_image_size('square', 150, 150, true);
+        add_image_size('sidebar', 470, 353, true);
 
-
-        if ( is_admin() ) {
+        if (is_admin()) {
             // Add image sizes to the editor
-            add_filter( 'image_size_names_choose', [ $this, 'add_image_sizes_editor' ] );
+            add_filter('image_size_names_choose', [$this, 'add_image_sizes_editor']);
         }
     }
 
-    public function add_image_sizes_editor( $sizes ) {
-        return array_merge( $sizes, [
-            'featured' => __( 'Featured', 'axe' ),
-            'gallery'  => __( 'Gallery', 'axe' ),
-            'grid'     => __( 'Grid', 'axe' ),
-            'square'   => __( 'Square', 'axe' ),
-            'sidebar'  => __( 'Sidebar', 'axe' ),
-            'post'     => __( 'Post' ),
-        ] );
+    public function add_image_sizes_editor($sizes)
+    {
+        return array_merge($sizes, [
+            'featured'   => __('Featured', 'axe'),
+            'gallery'    => __('Gallery', 'axe'),
+            'grid'       => __('Grid', 'axe'),
+            'square'     => __('Square', 'axe'),
+            'sidebar' => __('Sidebar', 'axe'),
+        ]);
     }
 }
