753828928be79584829a3165381dc68892000299
Adam Patterson <adam@bubbleupmarketing.ca>
2021-06-04 09:21:16 -0600

    Update search template

--- search.php
+++ search.php
@@ -1,4 +1,5 @@
 <?php
+
 $data = get_fields();
 $post = get_post();
 
@@ -6,11 +7,11 @@ include(get_template_part_acf('templates/partials/header'));
 
 echo '<!-- master/search -->';
 if (have_posts()):
-    echo '<!-- template: index/search -->';
-    get_template_part('templates/content', 'search');
+    echo '<!-- template: search/search -->';
+    include(get_template_part_acf('templates/content', 'search'));
 else:
-    echo '<!-- template: index/no_posts -->';
-    get_template_part('templates/none');
+    echo '<!-- template: search/no_posts -->';
+    include(get_template_part_acf('templates/none'));
 endif;
 
 include(get_template_part_acf('templates/partials/footer'));
