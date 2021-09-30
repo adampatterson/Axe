b66539e9ca41c7247d906fb33bdede103cd03031
Adam Patterson <hello@adampatterson.ca>
2021-09-29 19:42:38 -0600

    removed GA hard code, added get acf part, updated base css include

--- templates/partials/header.php
+++ templates/partials/header.php
@@ -8,38 +8,24 @@
 
     <script type='application/ld+json'>{"@context":"http:\/\/schema.org","@type":"WebSite","url":"<?= home_url('/'); ?>
         ","name":"<?= bloginfo('name') ?>","potentialAction":{"@type":"SearchAction","target":"<?= home_url('/'); ?>?s={search_term}","query-input":"required name=search_term"}}
+
+
     </script>
 
     <?php wp_head(); ?>
-    <link rel="stylesheet" href="<?= mix('/assets/css/base.css') ?>"/>
+    <link rel="stylesheet" href="<?= mix('/assets/css/base.css', true) ?>"/>
 </head>
 <?php
 $post = ( ! isset($post)) ? null : $post;
 if (is_null($post)) {
-    $post            = (object)$post;
+    $post            = (object) $post;
     $post->post_name = '';
 }
 ?>
 <body <?php body_class($post->post_name); ?>>
-<script>
-    (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r
-        i[r] = i[r] || function () {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date()
-        a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0]
-        a.async = 1
-        a.src = g
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga')
-
-    ga('create', '{@todo: GA Tracking}', 'auto')
-    ga('send', 'pageview')
-</script>
 <div id="wrapper">
-    <?php get_template_part('templates/partials/navigation'); ?>
+    <?php include(get_template_part_acf('templates/partials/navigation')); ?>
 
-    <?php get_template_part('templates/partials/custom-header'); ?>
+    <?php include(get_template_part_acf('templates/partials/custom-header')); ?>
 
 
