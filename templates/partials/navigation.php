08d03f15daaaaa74a13926634d3f66660c79df94
Adam Patterson <hello@adampatterson.ca>
2021-09-30 23:33:24 -0600

    #57 Updated Nav and Search form for BS5

--- templates/partials/navigation.php
+++ templates/partials/navigation.php
@@ -1,23 +1,24 @@
 <div class="navbar-wrapper pb-5">
-    <nav class="navbar navbar-expand-lg justify-content-between navbar-light bg-light" id="header">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" id="header">
         <div class="container">
 
             <?= get_the_logo(true, 'site-logo', 'navbar-brand mr-3'); ?>
 
             <a class="navbar-brand mr-auto" href="<?= home_url('/') ?>">
                 <h3 class="site-title mb-0"><?php bloginfo('name'); ?></h3>
-                <p class="site-description mb-0"><?php echo get_bloginfo('description', 'display'); ?></p>
             </a>
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu"
+                    aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <nav class="collapse navbar-collapse" id="menu">
+
+            <div class="collapse navbar-collapse" id="menu">
                 <?php
                 // http://code.tutsplus.com/tutorials/how-to-integrate-bootstrap-navbar-into-wordpress-theme--wp-33410
                 // https://github.com/jeffmould/multi-level-bootstrap-menu/blob/master/wp-bootstrap-navwalker.php
                 if (has_nav_menu('main-menu')) {
-                    wp_nav_menu(array(
+                    wp_nav_menu([
                         'menu'           => 'main-menu',
                         'theme_location' => 'main-menu',
                         'menu_id'        => 'navigation',
@@ -26,11 +27,12 @@
                         'escape_titles'  => false,
                         'menu_class'     => 'nav navbar-nav primary-nav ml-auto',
                         'walker'         => new Axe\Core\Walker()
-                    ));
+                    ]);
                 }
-                //            get_search_form();
+                get_search_form();
                 ?>
-            </nav>
+            </div>
+
         </div>
     </nav>
 </div>
