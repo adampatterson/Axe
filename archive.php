7d27fa64d0d7854610f141da3e1ea081c2cc1631
Adam Patterson <hello@adampatterson.ca>
2021-10-19 09:19:53 -0600

    Quick and simple Author archive

--- archive.php
+++ archive.php
@@ -14,12 +14,15 @@ if (have_posts()):
 
         if ($post_type) {
             $post_type_data = get_post_type_object($post_type);
-            $post_type_slug = $post_type_data->rewrite['slug'];
+            $post_type_slug = ($post_type !== 'post') ? $post_type_data->rewrite['slug'] : '';
         }
 
-        if (check_path('/templates/archive-' . $post_type_slug . '.php')):
-            echo '<!-- template: index/archive-' . $post_type_slug . ' -->';
+        if (check_path('/templates/archive-'.$post_type_slug.'.php')):
+            echo '<!-- template: index/archive-'.$post_type_slug.' -->';
             include(get_template_part_acf('templates/archive', $post_type_slug));
+        elseif (is_author()):
+            echo '<!-- template: templates/archive-author -->';
+            include(get_template_part_acf('templates/archive', 'author'));
         else:
             echo '<!-- template: index/archive -->';
             include(get_template_part_acf('templates/archive', 'default'));
