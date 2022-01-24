321c0b8ae024943ac4d7d4d4a31d8a9840416696
Adam Patterson <hello@adampatterson.ca>
2022-01-23 17:21:48 -0700

    Update data.php

--- lib/data.php
+++ lib/data.php
@@ -11,6 +11,10 @@ $post = get_post();
 /*
 Add a field group in ACF Options to keep the data nice and clean,
 then add a key to /theme/lib/Core/Options.php
+
+Ideally to the child theme:
+https://github.com/adampatterson/Handle/blob/staging/lib/Custom/Options.php
 */
 
-//$options = get_field('key', 'option');
+// $options = get_field('key', 'option');
+// $options     = Axe\Core\Options::get();
