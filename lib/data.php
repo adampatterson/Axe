c89d169db7b474fe38d58dd20d738f6fd1fd858f
Adam Patterson <hello@adampatterson.ca>
2022-01-23 17:19:37 -0700

    Update data.php

--- lib/data.php
+++ lib/data.php
@@ -1,5 +1,16 @@
 <?php
+
+if ( ! function_exists('get_fields')) {
+    echo 'Please install and enable ACF.';
+    die;
+}
+
 $data = get_fields();
 $post = get_post();
+
+/*
+Add a field group in ACF Options to keep the data nice and clean,
+then add a key to /theme/lib/Core/Options.php
+*/
 
 //$options = get_field('key', 'option');
