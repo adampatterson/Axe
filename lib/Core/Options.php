86a5a024bb443a7bffd1ea2d2f8de32df3aa6f1b
Adam Patterson <hello@adampatterson.ca>
2023-02-02 19:16:49 -0700

    simple content models.

--- lib/Core/Options.php
+++ lib/Core/Options.php
@@ -11,4 +11,19 @@ class Options
             acf_add_options_sub_page('General Settings');
         }
     }
+
+    static function get($key = null)
+    {
+        $options = [
+//            'contact'  => get_field('contact', 'option'),
+//            'theme'    => get_field('theme', 'option'),
+//            '404_page'      => get_field('404', 'option'),
+        ];
+
+        if (_has($options, $key)) {
+            return $options[$key];
+        }
+
+        return $options;
+    }
 }
