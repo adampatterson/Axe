daa1f7fd12de13be6cc1bfd28f1f5734e5fcfb21
Adam Patterson <adam@paper-leaf.com>
2023-05-03 10:41:20 -0600

    #51

--- lib/Custom.php
+++ lib/Custom.php
@@ -8,13 +8,19 @@ class Custom
     {
         // Places a link under Appearance in order to manage reusable Gutenberg Blocks.
         add_action('admin_menu', [$this, 'add_custom_link_into_appearance_menu']);
+        //        add_filter('acf/settings/save_json', [$this, 'acf_save']);
     }
 
     public function add_custom_link_into_appearance_menu()
     {
         global $submenu;
-        $blockLink             = 'edit.php?post_type=wp_block';
+        $blockLink = 'edit.php?post_type=wp_block';
         $submenu['edit.php'][] = array('Edit Reusable Blocks', 'manage_options', $blockLink);
+    }
+
+    public function acf_save()
+    {
+        return get_template_directory() . '/acf-json';
     }
 
 }
