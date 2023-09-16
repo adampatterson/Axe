d5c1638740c2d7c31871fb80dfbb78fb52a7c5bc
Adam Patterson <hello@adampatterson.ca>
2023-09-16 16:46:52 -0600

    Sidebar cleanup

--- templates/woo-single.php
+++ templates/woo-single.php
@@ -2,11 +2,7 @@
     <div class="container pt-5">
 
         <div class="row">
-            <?php if (is_active_sidebar('woo-single')) { ?>
-            <div class="col-md-8">
-            <?php } else { ?>
-            <div class="col-md-12">
-            <?php } ?>
+            <div class="<?= (is_active_sidebar('woo-single')) ? 'col-md-8' : 'col-md-12' ?>">
                 <section id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                     <div class="wrapper clearfix">
                         <?php the_title('<header class="entry-header"><h1 class="entry-title">', '</h1></header><!-- .entry-header -->'); ?>
