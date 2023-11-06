1f7e867d57a1a59a697351dae9345a4a933853db
Adam Patterson <adam@paper-leaf.com>
2023-11-06 11:02:24 -0700

    .

--- lib/data.php
+++ lib/data.php
@@ -3,4 +3,4 @@
 $data        = Axe\Core\Content::getFields();
 $post        = Axe\Core\Content::getPosts();
 $options     = Axe\Core\Content::getOptions();
-$mainOptions = (new \Axe\Core\Network)->getMainSite()['options'];
+// $mainOptions = (new \Axe\Core\Network)->getMainSite()['options']; // Uncomment if you're using a WordPress Multi Site
