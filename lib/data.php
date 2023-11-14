945df42a2ed7804ec9297659fe1e7921785f9339
Adam Patterson <hello@adampatterson.ca>
2023-11-14 01:51:41 -0700

    .

--- lib/data.php
+++ lib/data.php
@@ -19,4 +19,4 @@ https://github.com/adampatterson/Handle/blob/staging/lib/Custom/Options.php
 // $options    = get_field('key', 'option');
 $options    = Axe\Models\Options::get();
 
- $mainOptions = (new \Axe\Core\Network)->getMainSite()['options']; // Uncomment if you're using a WordPress Multi Site
+// $mainOptions = (new \Axe\Core\Network)->getMainSite()['options']; // Uncomment if you're using a WordPress Multi Site
