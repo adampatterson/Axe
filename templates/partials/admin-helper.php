3c868b0bdf8f09600b537dd297327c73edc85fee
Adam Patterson <hello@adampatterson.ca>
2024-03-02 12:03:44 -0700

    Admin helper optimally uses ray

--- templates/partials/admin-helper.php
+++ templates/partials/admin-helper.php
@@ -1,51 +1,36 @@
 <?php
 if (is_user_logged_in()):
-    if (isset($data)): ?>
-        <script>
-            window.data = <?= json_encode($data) ?>
-
-            // ACF Data
-            console.log("ACF Data:", window.data)
-        </script>
-    <?php endif;
-    if (isset($options)): ?>
-        <script>
-            window.options = <?= json_encode($options) ?>;
-
-            // ACF Data
-            console.log("ACF Options:", window.options);
-        </script>
-    <?php endif;
-
-    if (isset($post)): ?>
-        <script>
-            window.post = <?= json_encode($post) ?>
-            // Post Data
-            console.log("Post:", window.post)
-        </script>
-    <?php endif;
-
-    if (isset($products)): ?>
-        <script>
-            window.product = <?= json_encode($products) ?>
-            // is product
-            console.log("Product:", window.product)
-        </script>
-    <?php endif;
-    if (class_exists('WooCommerce')): ?>
-        <script>
-            // Is WooCommerce Installed
-            window.woo = <?= json_encode([
-                'is_shop'             => is_shop(),
-                'is_product_category' => is_product_category(),
-                'is_product_tag'      => is_product_tag(),
-                'is_product'          => is_product(),
-                'is_cart'             => is_cart(),
-                'is_checkout'         => is_checkout(),
-                'is_account_page'     => is_account_page(),
-                'template_file'       => show_template()
-            ]); ?>;
-            console.log("WooCommerce:", window.woo)
-        </script>
-    <?php endif;
-endif; ?>
+    include(__THEME_DATA__.'/lib/data.php');
+    $debugData = [
+        'acf_data'        => isset($data) ? $data : [],
+        'options'         => isset($options) ? $options : [],
+        'post'            => isset($post) ? $post : [],
+        'products'        => isset($products) ? $products : [],
+        'get_post_type'   => get_post_type(),
+        'get_post_format' => get_post_format(),
+        'template_file'   => show_template(),
+        'is'              => [
+            'is_front_page'       => is_front_page(),
+            'is_single'           => is_single(),
+            'is_archive'          => is_archive(),
+            'is_author'           => is_author(),
+            'is_page'             => is_page(),
+            'is_shop'             => is_shop(),
+            'is_product_category' => is_product_category(),
+            'is_product_tag'      => is_product_tag(),
+            'is_product'          => is_product(),
+            'is_cart'             => is_cart(),
+            'is_checkout'         => is_checkout(),
+            'is_account_page'     => is_account_page(),
+            'is_attachment'       => is_attachment(),
+        ]
+    ];
+    if (function_exists('ray')) {
+        ray($debugData);
+    }
+    ?>
+    <script>
+        window.debugData = <?= json_encode($debugData) ?>;
+        console.log(window.debugData);
+    </script>
+<?php endif; ?>
