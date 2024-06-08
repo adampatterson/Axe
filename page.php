ec95e967bf2f2e42e2997493170fada3566e5996
Adam Patterson <hello@adampatterson.ca>
2024-06-08 14:44:43 -0600

    changed get_acf_part  to get_template_part_acf

--- page.php
+++ page.php
@@ -5,43 +5,44 @@
 
 include(__THEME_DATA__.'/lib/data.php');
 
-get_acf_part('templates/partials/header');
+include get_template_part_acf('templates/partials/header');
 
 echo '<!-- main/page -->';
 while (have_posts()) : the_post();
     if (is_front_page()):
         echo '<!-- template: templates/content-home -->';
-        get_acf_part('templates/content', 'home');
+        include get_template_part_acf('templates/content', 'home');
     else:
         if (is_sub_page($post)):
             $patent = get_post($post->post_parent);
             if (check_path('/templates/sub-'.$post->post_name.'.php')):
                 echo '<!-- template: templates/sub-'.$post->post_name.' -->';
-                get_acf_part('templates/sub', $post->post_name);
+                include get_template_part_acf('templates/sub', $post->post_name);
             elseif (check_path('/templates/content-'.$patent->post_name.'.php')):
                 echo '<!-- template: templates/content-'.$patent->post_name.' -->';
-                get_acf_part('templates/content', $patent->post_name);
+                include get_template_part_acf('templates/content', $patent->post_name);
             else:
                 echo '<!-- template: templates/content-page.php -->';
-                get_acf_part('templates/content', 'page');
+                include get_template_part_acf('templates/content', 'page');
             endif;
-//      Single Page
+        // Single Page
         elseif (check_path('/templates/content-'.$post->post_name.'.php')):
             echo '<!-- template: templates/content-'.$post->post_name.' -->';
-            get_acf_part('templates/content', $post->post_name);
-
-//      WooCommerce Product Listing
+            include get_template_part_acf('templates/content', $post->post_name);
+        // WooCommerce Product Listing
         elseif (show_woo_listing()):
             echo '<!-- template: templates/woo-listing -->';
-            get_acf_part('templates/woo', 'listing');
+            include get_template_part_acf('templates/woo', 'listing');
         elseif (show_woo_category()):
             echo '<!-- template: templates/woo-category -->';
-            get_acf_part('templates/woo', 'category');
+            include get_template_part_acf('templates/woo', 'category');
         else:
             echo '<!-- template: templates/content-page -->';
-            get_acf_part('templates/content', 'page');
+            include get_template_part_acf('templates/content', 'page');
         endif;
     endif;
 endwhile;
 
-get_acf_part('templates/partials/footer');
+include get_template_part_acf('templates/partials/footer');
+
+
